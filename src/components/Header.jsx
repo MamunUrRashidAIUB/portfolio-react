966ba1b6ee4358edc6ef2242343e226311b62948
Mamun ur rashid <md.mamun.ur.rashid.cse@gmail.com>
2024-10-12 13:35:35 +0600

    nav bar done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,12 @@ import Logo from "./Logo";
 import Nav from "./Nav";
 
 const Header = () => {
-  return <div className="bg-black sticky top-0 z-20 mx-auto flex w-full items-center justify-between text-white">
-  <Logo/>
-   <Nav/>
-   </div>;
+  return (
+    <div className="bg-black sticky top-0 z-[20] mx-auto flex flex-wrap w-full items-center justify-between text-white">
+      <Logo />
+      <Nav />
+    </div>
+  );
 };
 
 export default Header;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,30 +1,35 @@
-import  { useState } from "react";
+import { useState } from "react";
 
 const Nav = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const togglenavbar = () => setIsOpen(!isOpen);
+  const toggleNavbar = () => setIsOpen(!isOpen);
+
   return (
-    <div className="">
-      <div className="flex space-x-4 md:space-x-10 lg:space-x-20 pr-6 md:flex hidden ">
-        <a href="home"> Home</a>
-        <a href="about">About</a>
-        <a href="projects">Projects </a>
-        <a href="blogs"> Blogs</a>
-        <a href="contact"> Contact</a>
+    <div className="relative">
+      <div className="hidden md:flex space-x-4 md:space-x-10 lg:space-x-20 pr-8">
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#projects">Projects</a>
+        <a href="#blogs">Blogs</a>
+        <a href="#contact">Contact</a>
       </div>
-      <div className="md:hidden pr-6">
-        <button onClick={togglenavbar}>{isOpen ? "X" :"|||"}</button>
+      <button
+        onClick={toggleNavbar}
+        className="text-white text-2xl pt-4 pr-4 flex md:hidden items-center justify-end w-full"
+      >
+        {isOpen ? "✕" : "|||"}
+      </button>
+      <div
+        className={`md:hidden flex basis-full flex-col items-center space-y-4 text-white p-4 transition-all duration-700 ease-in-out transform ${
+          isOpen ? "opacity-100 max-h-screen" : "opacity-0 max-h-0"
+        }`}
+      >
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#projects">Projects</a>
+        <a href="#blogs">Blogs</a>
+        <a href="#contact">Contact</a>
       </div>
-
-{isOpen && ( <div className="md:hidden flex flex-col space-y-4 bg-black text-white p-4 translate-x-0">
-          <a href="#home">Home</a>
-          <a href="#about">About</a>
-          <a href="#projects">Projects</a>
-          <a href="#blogs">Blogs</a>
-          <a href="#contact">Contact</a>
-        </div>)}
-
-
     </div>
   );
 };
