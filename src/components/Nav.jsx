7ab0dac7a6b6ab1ff98b886edc276162a723085e
Mamun ur rashid <md.mamun.ur.rashid.cse@gmail.com>
2024-10-12 12:26:31 +0600

    nav menu done for all scree

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,10 @@
 const Nav = () => {
+const
+
+
+
   return (
-    <div className="flex space-x-20">
+    <div className="flex space-x-4 md:space-x-10 lg:space-x-20 pr-6">
       <a href=""> Home</a>
       <a href="">About</a>
       <a href="">Projects </a>
