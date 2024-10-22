b96d0b56ac075dbfad793ac5a84e5b21f807b1e0
Mamun ur rashid <md.mamun.ur.rashid.cse@gmail.com>
2024-10-23 01:14:24 +0600

    some changes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,22 +1,38 @@
-import portfoliodata from "../portfolio.json"
+import React from "react";
+import portfoliodata from "../portfolio.json";
 
 const Projects = () => {
   return (
     <div className="p-4 bg-black text-white">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
-        {portfoliodata.map(project => (
-          <div key={project.id} className=" p-1 rounded-lg shadow-lg border-cyan-400">
-            <img src={`../src/Photos/${project.image}`} alt={project.title} className="w-full h-48 object-cover mb-2" />
+        {portfoliodata.map((project) => (
+          <div
+            key={project.id}
+            className=" p-1 rounded-lg shadow-lg border-cyan-400"
+          >
+            <img
+              src={`../src/Photos/${project.image}`}
+              alt={project.title}
+              className="w-full h-48 object-cover mb-2"
+            />
             <h3 className="text-xl font-semibold">{project.title}</h3>
             <p>{project.description}</p>
-            <div className="flex space-x-2 my-2">
+            <div className="flex space-x-2 my-2 pb-2 ">
               {project.technologies.map((tech, index) => (
-                <span key={index} className="bg-black text-white border-cyan-400 border px-2 py-1 rounded hover:bg-cyan-400 hover:text-black">
+                <span
+                  key={index}
+                  className="bg-black text-white border-cyan-400 border px-2 py-1 rounded transition-colors duration-900 ease-in-out "
+                >
                   {tech}
                 </span>
               ))}
             </div>
-            <a href={project.link} target="_blank" rel="noopener noreferrer" className="text-cyan-400  hover:text-stone-100">
+            <a
+              href={project.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className=" bg-cyan-400 text-black border-cyan-400 border px-2 py-1 rounded relative overflow-hidden transition-colors duration-500 ease-in-out hover:text-white hover:bg-gradient-to-r hover:from-cyan-400 hover:to-black"
+            >
               View Project
             </a>
           </div>
@@ -26,4 +42,3 @@ const Projects = () => {
   );
 };
 export default Projects;
-
