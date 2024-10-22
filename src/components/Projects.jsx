a9542320d1c4f786f4e06ecd7b88d93b7bcc4515
Mamun ur rashid <md.mamun.ur.rashid.cse@gmail.com>
2024-10-23 00:35:04 +0600

    some changes in style

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,20 +3,20 @@ import portfoliodata from "../portfolio.json"
 const Projects = () => {
   return (
     <div className="p-4 bg-black text-white">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
         {portfoliodata.map(project => (
-          <div key={project.id} className="border p-4 rounded-lg shadow-lg border-cyan-400">
+          <div key={project.id} className=" p-1 rounded-lg shadow-lg border-cyan-400">
             <img src={`../src/Photos/${project.image}`} alt={project.title} className="w-full h-48 object-cover mb-2" />
             <h3 className="text-xl font-semibold">{project.title}</h3>
             <p>{project.description}</p>
             <div className="flex space-x-2 my-2">
               {project.technologies.map((tech, index) => (
-                <span key={index} className="bg-cyan-300 text-black px-2 py-1 rounded">
+                <span key={index} className="bg-black text-white border-cyan-400 border px-2 py-1 rounded hover:bg-cyan-400 hover:text-black">
                   {tech}
                 </span>
               ))}
             </div>
-            <a href={project.link} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">
+            <a href={project.link} target="_blank" rel="noopener noreferrer" className="text-cyan-400  hover:text-stone-100">
               View Project
             </a>
           </div>
