47250d289ebbaf0e36fd8fe1d858bb954773efdb
Mamun ur rashid <md.mamun.ur.rashid.cse@gmail.com>
2024-10-23 18:47:17 +0600

    footer section done

---
+++ src/components/Connectwithme.jsx
@@ -0,0 +1,48 @@
+import { renderAnimatedText } from "./TextAnimation";
+import Lottie from "lottie-react";
+import animation from "./animation/facebook.json";
+import animation2 from "./animation/github.json";
+
+
+const ConnectWithMe = () => {
+  const text1 = "Connect";
+  const text2 = "With";
+  const text3 = "Me";
+  return (
+    <div>
+      <div>
+        <h1 className="text-4xl text-center uppercase text-cyan-400 sm:text-6xl md:text-8xl lg:text-6xl font-bold pb-5">
+          {renderAnimatedText(text1)}
+          <span className="mx-2"></span>
+          {renderAnimatedText(text2)}
+          <span className="mx-2"></span>
+          {renderAnimatedText(text3)}
+        </h1>
+      </div>
+      <div className="bg-slate-900 border border-cyan-400 rounded-md font-bold text-white p-5">
+        <div className="flex justify-center items-center">
+          <a href="https://www.facebook.com/profile.php?id=100026601601848" target="_blank" rel="noopener noreferrer">
+            <Lottie animationData={animation} className="w-10 h-14" />
+          </a>
+          <a href="https://github.com/MamunUrRashidAIUB" target="_blank" rel="noopener noreferrer">
+            <Lottie animationData={animation2} className="w-10 h-14" />
+          </a>
+        </div>
+        <div className="flex flex-col justify-center items-center">
+          <span>
+            Email:
+            <a href="mailto:md.mamun.ur.rashid.cse@gmail.com">
+              md.mamun.ur.rashid.cse@gmail.com
+            </a>
+          </span>
+          <p>Phone: +880 1880 299 555</p>
+          <p className="text-sm">
+            &copy; {new Date().getFullYear()} All Rights Reserved. | Developed by Rashid
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ConnectWithMe;
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,5 @@
-
-
 import Button from "./Button";
+import Connectwithme from "./Connectwithme";
 import { renderAnimatedText } from "./TextAnimation";
 
 const Contact = () => {
@@ -11,7 +10,7 @@ const Contact = () => {
     <section id="contact" className="bg-black text-white p-6 sm:p-12">
       <div className="flex flex-col md:flex-row">
         <div className="contact-form bg-black p-6 rounded-lg shadow-lg md:w-1/2 md:mr-4">
-          <h2 className="text-4xl text-cyan-400 sm:text-6xl md:text-8xl lg:text-6xl font-bold pb-5">
+          <h2 className="uppercase text-4xl text-cyan-400 sm:text-6xl md:text-8xl lg:text-6xl font-bold pb-5">
             {renderAnimatedText(text1)}
             <span className="mx-2"></span>
             {renderAnimatedText(text2)}
@@ -52,10 +51,9 @@ const Contact = () => {
               required
               className="w-full p-2 mb-4 border border-cyan-600 rounded bg-black text-white placeholder-gray-400 focus:outline-none focus:border-cyan-400"
             ></textarea>
-<div className="p-6 pl-10 ">
-<Button className="" text="Send" />
-</div>
-           
+            <div className="p-6 pl-10 ">
+              <Button className="" text="Send" />
+            </div>
           </form>
         </div>
 
@@ -71,15 +69,22 @@ const Contact = () => {
           ></iframe>
           {/* Location Details Overlay */}
           <div className="absolute top-4 left-4 bg-cyan-400 bg-opacity-80 p-4 rounded-lg shadow-md text-black text-sm sm:text-base sm:top-4 sm:left-4">
-            <p><strong>Location:</strong>Bashundhora , Dhaka ,Bangladesh</p>
-            <p><strong>Phone:</strong>+880 1880299555</p>
-            <p><strong>Email:</strong>md.mamun.ur.rashid.cse@gmail.com</p>
+            <p>
+              <strong>Location:</strong>Bashundhora , Dhaka ,Bangladesh
+            </p>
+            <p>
+              <strong>Phone:</strong>+880 1880299555
+            </p>
+            <p>
+              <strong>Email:</strong>md.mamun.ur.rashid.cse@gmail.com
+            </p>
           </div>
         </div>
+       
       </div>
+      <Connectwithme />
     </section>
   );
 };
 
 export default Contact;
-
