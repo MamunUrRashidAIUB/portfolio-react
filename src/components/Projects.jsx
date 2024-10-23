dfd10d3ae118ee66059b9768e05d1b84d0f1db2b
Mamun ur rashid <md.mamun.ur.rashid.cse@gmail.com>
2024-10-23 16:35:30 +0600

    button style changes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,5 @@
-
+import { map, image, title, link } from "framer-motion/client";
+import React from "react";
 import portfoliodata from "../portfolio.json";
 
 const Projects = () => {
@@ -8,13 +9,16 @@ const Projects = () => {
         {portfoliodata.map((project) => (
           <div
             key={project.id}
-            className=" p-1 rounded-lg shadow-lg border-cyan-400"
+            className="group relative p-1 rounded-lg shadow-lg border-cyan-400 overflow-hidden"
           >
             <img
               src={`../src/Photos/${project.image}`}
               alt={project.title}
-              className="w-full h-48 object-cover mb-2"
+              className="w-full h-48 object-cover mb-2 transition-transform duration-500 ease-in-out group-hover:blur-sm group-hover:scale-105"
             />
+
+            <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-50 transition-opacity duration-500"></div>
+
             <h3 className="text-xl font-semibold">{project.title}</h3>
             <p>{project.description}</p>
             <div className="flex space-x-2 my-2 pb-2 ">
@@ -27,18 +31,24 @@ const Projects = () => {
                 </span>
               ))}
             </div>
-            <a
-              href={project.link}
-              target="_blank"
-              rel="noopener noreferrer"
-              className=" bg-cyan-400 text-black border-cyan-400 border px-2 py-1 rounded relative overflow-hidden transition-colors duration-500 ease-in-out hover:text-white hover:bg-gradient-to-r hover:from-cyan-400 hover:to-black"
-            >
-              View Project
-            </a>
+
+            <div className="opacity-0 group-hover:opacity-100 absolute justify-center left-1/2 top-1/3 transform -translate-x-1/2 -translate-y-1/2">
+              <a
+                href={project.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="px-5 py-2.5 relative rounded group overflow-hidden font-medium bg-purple-50 text-purple-600 inline-block"
+              >
+                <span className="absolute top-0 left-0 flex w-0 h-full transition-all duration-300 ease-out transform group-hover:w-full bg-cyan-400 opacity-90"></span>
+                <span className="relative group-hover:text-black">View Project</span>
+              </a>
+            </div>
           </div>
         ))}
       </div>
     </div>
   );
 };
+
 export default Projects;
+
