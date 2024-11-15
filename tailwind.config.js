5d92e4c173aa38ff3ad86fa5d07f2eadc22367bc
Md. Mamun ur rashid <md.mamun.ur.rashid.cse@gmail.com>
2024-11-16 04:43:38 +0600

    some changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        oswald: ["Oswald", "sans-serif"], // Add Oswald as a custom font
+      },
       animation: {
         marquee: "marquee 20s linear infinite", // Slower animation speed across all screens
       },
