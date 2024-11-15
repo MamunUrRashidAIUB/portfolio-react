3b362cd1aaa249309f92b9f26e749be5f29a7823
Md. Mamun ur rashid <md.mamun.ur.rashid.cse@gmail.com>
2024-11-16 01:07:06 +0600

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import MyPortfolio from "./components/MyPortfolio";
 import Projects from "./components/Projects";
 import Currentwork from "./components/Currentwork";
 import Contact from "./components/Contact";
-import ConnectWithMe from "./components/Connectwithme";
+
 import { Switch, Route } from "react-router-dom";
 import About from "./components/About";
 import Skills from "./components/Skills";
