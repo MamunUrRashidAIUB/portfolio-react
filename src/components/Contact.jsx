31f00713aaddc70f7d62e6a14e35e239ee0d9fbe
Md. Mamun ur rashid <md.mamun.ur.rashid.cse@gmail.com>
2024-11-16 03:18:24 +0600

    positon settings in  contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,6 +12,7 @@ const Contact = () => {
       id="contact"
       className="min-h-screen bg-black p-1 text-white sm:p-12"
     >
+      <ConnectWithMe />
       <div className="flex flex-col md:flex-row">
         <div className="contact-form rounded-lg bg-black p-6 shadow-lg md:mr-4 md:w-1/2">
           <h2 className="pb-5 text-4xl font-bold uppercase text-cyan-400 sm:text-6xl md:text-4xl lg:text-6xl">
@@ -90,7 +91,6 @@ const Contact = () => {
           </div>
         </div>
       </div>
-      <ConnectWithMe />
     </section>
   );
 };
